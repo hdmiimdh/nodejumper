6f85d9f5f1a52a65631d1ae5f58981c3da469065
RefleXXX | NodeJumper <elena.lebedeeva@gmail.com>
2022-04-02 21:20:06 +0300

    Remove rpc2 as it seems to be redundant

--- src/app/model/chain.ts
+++ src/app/model/chain.ts
@@ -3,10 +3,8 @@ export class Chain {
   chainName: string;
   chainId: string;
   snapshotServer: string;
-  rpcServer1: string;
-  rpcServer2: string;
-  peer1: string;
-  peer2: string;
+  rpcServer: string;
+  peer: string;
   serviceName: string;
   binaryName?: string;
   homeDirectoryName: string;
@@ -18,15 +16,13 @@ export class Chain {
   coingekoCoinId: string;
   validatorUrl: string;
 
-  constructor(id: string, chainName: string, chainId: string, snapshotServer: string, rpcServer1: string, rpcServer2: string, peer1: string, peer2: string, serviceName: string, homeDirectoryName: string, twitter: string, github: string, globe: string, medium: string, coingekoCoinId: string, validatorUrl: string) {
+  constructor(id: string, chainName: string, chainId: string, snapshotServer: string, rpcServer: string, peer: string, serviceName: string, homeDirectoryName: string, twitter: string, github: string, globe: string, medium: string, coingekoCoinId: string, validatorUrl: string) {
     this.id = id;
     this.chainName = chainName;
     this.chainId = chainId;
     this.snapshotServer = snapshotServer;
-    this.rpcServer1 = rpcServer1;
-    this.rpcServer2 = rpcServer2;
-    this.peer1 = peer1;
-    this.peer2 = peer2;
+    this.rpcServer = rpcServer;
+    this.peer = peer;
     this.serviceName = serviceName;
     this.homeDirectoryName = homeDirectoryName;
     this.twitter = twitter;
