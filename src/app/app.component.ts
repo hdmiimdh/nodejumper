70ff2ffe5b3097d325c3a688436d27b249a589bd
RefleXXX | NodeJumper <elena.lebedeeva@gmail.com>
2022-04-28 23:20:52 +0300

    Close left-hand side menu by clicking outside of menu on mobile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { DOCUMENT } from "@angular/common";
+import { LeftHandMenuService } from "./service/left-hand-menu.service";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'nodejumper';
+
+  constructor(@Inject(DOCUMENT) private document: Document,
+              private leftHandMenuService: LeftHandMenuService) {
+  }
+
+  closeLeftHandMenuForMobile(): void {
+    let body = document.getElementsByTagName('body');
+    if (body && body.length && body[0].classList.contains('left-hand-menu-open')) {
+      this.leftHandMenuService.closeLeftHandMenuForMobile();
+    }
+  }
 }
