97355fc5e80360c3803e73c526c0ac6867b23bcd
RefleXXX | NodeJumper <elena.lebedeeva@gmail.com>
2022-06-29 21:05:55 +0400

    Disable browser cache for snapshot info.json

--- src/app/chain-detail-page/synchronization-scripts/synchronization-scripts.component.ts
+++ src/app/chain-detail-page/synchronization-scripts/synchronization-scripts.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Chain } from "../../model/chain";
 import { HighlightService } from "../../service/highlight.service";
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { ChainService } from "../../service/chain.service";
 
 @Component({
@@ -29,7 +29,11 @@ export class SynchronizationScriptsComponent implements OnInit {
     if (this.chain) {
       let id = this.chain.id;
       let snapshotStateFileLocation = this.chain?.snapshotServer + '/' + id + '/current_state.json';
-      this.http.get(snapshotStateFileLocation)
+      let headers = new HttpHeaders();
+      headers.set('Cache-Control', 'no-cache, no-store, must-revalidate, post-check=0, pre-check=0');
+      headers.set('Pragma', 'no-cache');
+      headers.set('Expires', '0');
+      this.http.get(snapshotStateFileLocation, {headers: headers})
         .subscribe((data: any) => {
           this.snapshotHeight = data.snapshotHeight;
           this.snapshotSize = data.snapshotSize + 'B';
