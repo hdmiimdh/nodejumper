b022a6ceab852323f838b8d0b4e250a9ec809d4b
libra422 <libra.validator@gmail.com>
2022-07-02 22:19:11 +0700

    fix hashes for paloma genesis

--- src/assets/data/installation-scripts/manual/paloma-testnet.sh
+++ src/assets/data/installation-scripts/manual/paloma-testnet.sh
@@ -24,10 +24,9 @@ $binaryName config chain-id $chainId
 $binaryName init nodejumper --chain-id $chainId -o
 
 curl https://raw.githubusercontent.com/palomachain/testnet/master/paloma-testnet-6/genesis.json > $HOME/$homeDirectoryName/config/genesis.json
-sha256sum $HOME/$homeDirectoryName/config/genesis.json #
+sha256sum $HOME/$homeDirectoryName/config/genesis.json # bad4a385822e25d8357c6b0b6ba4f4ab7c47355b105d02397994f718fb9a407a
 
 curl https://raw.githubusercontent.com/palomachain/testnet/master/paloma-testnet-6/addrbook.json > $HOME/$homeDirectoryName/config/addrbook.json
-sha256sum $HOME/$homeDirectoryName/config/addrbook.json # d2cfeb63b4c926a69d63af65f537369dcae8e96bba92a422bcdfca5203f1304c
 
 sed -i 's|^minimum-gas-prices *=.*|minimum-gas-prices = "0.0001$denomName"|g' $HOME/$homeDirectoryName/config/app.toml
 seeds=""
