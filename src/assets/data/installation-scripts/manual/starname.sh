afe6c64ae0b9e09949167a19245f5cbb96797580
RefleXXX | NodeJumper <elena.lebedeeva@gmail.com>
2022-07-06 14:30:06 +0400

    Add build-essential to common packages

--- src/assets/data/installation-scripts/manual/starname.sh
+++ src/assets/data/installation-scripts/manual/starname.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt install -y make gcc jq curl git snapd
+sudo apt install -y make gcc jq curl git snapd build-essential
 sudo snap install lz4
 
 if [ ! -f "/usr/local/go/bin/go" ]; then
