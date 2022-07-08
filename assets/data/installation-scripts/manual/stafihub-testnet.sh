c3a05fff6a8d7fea2454fa45fccad8e276bd0410
RefleXXX1 <102145525+RefleXXX1@users.noreply.github.com>
2022-07-08 18:16:48 +0000

    Deploying to gh-pages from  @ 1fe9df2eb8bc971857904db16afdad064f9c4880 🚀

---
+++ assets/data/installation-scripts/manual/stafihub-testnet.sh
@@ -0,0 +1,116 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt install -y make gcc jq curl git build-essential
+
+if [ ! -f "/usr/local/go/bin/go" ]; then
+  . <(curl -s "https://raw.githubusercontent.com/nodejumper-org/cosmos-utils/main/utils/go_install.sh")
+  . .bash_profile
+fi
+
+go version # go version goX.XX.X linux/amd64
+
+cd || return
+rm -rf stafihub
+git clone https://github.com/stafihub/stafihub
+cd stafihub || return
+git checkout public-testnet-v3
+make install
+$binaryName version # nothing is printed
+
+# replace nodejumper with your own moniker, if you'd like
+$binaryName config chain-id $chainId
+$binaryName init nodejumper --chain-id $chainId
+
+curl https://raw.githubusercontent.com/stafihub/network/main/testnets/stafihub-public-testnet-3/genesis.json > $HOME/$homeDirectoryName/config/genesis.json
+sha256sum $HOME/$homeDirectoryName/config/genesis.json # 364d5c18b18d3a1d3fcc9125f855610f66c28b5df089ca1900376059273f4ef1
+
+sed -i 's|^minimum-gas-prices *=.*|minimum-gas-prices = "0.0001$denomName"|g' $HOME/$homeDirectoryName/config/app.toml
+seeds=""
+peers="$rpcPeer,e906c21307a875c743806f1a92ecb50b5138480d@65.21.138.123:30656,3a440f9fd1a9138393e395028bd6079a187364c6@65.108.124.172:26656"
+sed -i -e 's|^seeds *=.*|seeds = "'$seeds'"|; s|^persistent_peers *=.*|persistent_peers = "'$peers'"|' $HOME/$homeDirectoryName/config/config.toml
+
+# in case of pruning
+sed -i 's|pruning = "default"|pruning = "custom"|g' $HOME/$homeDirectoryName/config/app.toml
+sed -i 's|pruning-keep-recent = "0"|pruning-keep-recent = "100"|g' $HOME/$homeDirectoryName/config/app.toml
+sed -i 's|pruning-interval = "0"|pruning-interval = "10"|g' $HOME/$homeDirectoryName/config/app.toml
+
+sudo tee /etc/systemd/system/$serviceName.service > /dev/null << EOF
+[Unit]
+Description=$chainName Node
+After=network-online.target
+[Service]
+User=$USER
+ExecStart=$(which $binaryName) start
+Restart=on-failure
+RestartSec=10
+LimitNOFILE=10000
+[Install]
+WantedBy=multi-user.target
+EOF
+
+$binaryName tendermint unsafe-reset-all --home $HOME/$homeDirectoryName --keep-addr-book
+
+SNAP_RPC="$rpcServer"
+LATEST_HEIGHT=$(curl -s $SNAP_RPC/block | jq -r .result.block.header.height); \
+BLOCK_HEIGHT=$((LATEST_HEIGHT - 2000)); \
+TRUST_HASH=$(curl -s "$SNAP_RPC/block?height=$BLOCK_HEIGHT" | jq -r .result.block_id.hash)
+
+echo $LATEST_HEIGHT $BLOCK_HEIGHT $TRUST_HASH
+
+sed -i -E "s|^(enable[[:space:]]+=[[:space:]]+).*$|\1true| ; \
+s|^(rpc_servers[[:space:]]+=[[:space:]]+).*$|\1\"$SNAP_RPC,$SNAP_RPC\"| ; \
+s|^(trust_height[[:space:]]+=[[:space:]]+).*$|\1$BLOCK_HEIGHT| ; \
+s|^(trust_hash[[:space:]]+=[[:space:]]+).*$|\1\"$TRUST_HASH\"|" $HOME/$homeDirectoryName/config/config.toml
+
+sudo systemctl daemon-reload
+sudo systemctl enable $serviceName
+sudo systemctl restart $serviceName
+
+sudo journalctl -u $serviceName -f --no-hostname -o cat
+
+Create Validator Delimiter
+
+# Create wallet
+$binaryName keys add wallet
+
+## Console output
+#- name: wallet
+#  type: local
+#  address: stafi1wpkxhzufzrmz6glt4sjp54k3umgvx5hv3rx6y7
+#  pubkey: '{"@type":"/cosmos.crypto.secp256k1.PubKey","key":"Auq9WzVEs5pCoZgr2WctjI7fU+lJCH0I3r6GC1oa0tc0"}'
+#  mnemonic: ""
+
+#!!! SAVE SEED PHRASE
+kite upset hip dirt pet winter thunder slice parent flag sand express suffer chest custom pencil mother bargain remember patient other curve cancel sweet
+
+# Wait util the node is synced, should return FALSE
+$binaryName status 2>&1 | jq .SyncInfo.catching_up
+
+# Go to discord channel #stafi-hub-faucetw and paste
+!faucet send <YOUR_WALLET_ADDRESS>
+
+# Verify the balance
+$binaryName q bank balances $($binaryName keys show wallet -a)
+
+## Console output
+#  balances:
+#  - amount: "100000000"
+#    denom: ufis
+
+# Create validator
+$binaryName tx staking create-validator \
+--amount=99000000$denomName \
+--pubkey=$($binaryName tendermint show-validator) \
+--moniker=<YOUR_MONIKER_NAME> \
+--chain-id=$chainId \
+--commission-rate=0.1 \
+--commission-max-rate=0.2 \
+--commission-max-change-rate=0.05 \
+--min-self-delegation=1 \
+--fees=20000$denomName \
+--from=wallet \
+-y
+
+# Make sure you see the validator details
+$binaryName q staking validator $($binaryName keys show wallet --bech val -a)
