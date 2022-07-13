3db1bd1110d2b26a6cb28b09c81ad9bb286c9c9b
RefleXXX | NodeJumper <elena.lebedeeva@gmail.com>
2022-07-13 11:21:16 +0400

    Add chain state indicator Make validator param dynamic in cheat sheet

--- src/app/chain-detail-page/left-hand-menu/left-hand-menu.component.ts
+++ src/app/chain-detail-page/left-hand-menu/left-hand-menu.component.ts
@@ -1,7 +1,10 @@
 import { Component, Inject, Input, OnInit } from '@angular/core';
 import { Chain } from "../../model/chain";
+import { ChainStatus } from "../../model/chainStatus";
 import { DOCUMENT } from '@angular/common';
 import { LeftHandMenuService } from "../../service/left-hand-menu.service";
+import { ChainService } from "../../service/chain.service";
+import { UtilsService } from "../../service/utils.service";
 
 @Component({
   selector: 'app-left-hand-menu',
@@ -11,29 +14,59 @@ import { LeftHandMenuService } from "../../service/left-hand-menu.service";
 export class LeftHandMenuComponent implements OnInit {
 
   @Input() chain?: Chain;
+  chainStatus?: ChainStatus;
+  chainStatusMessage?: string;
 
   constructor(@Inject(DOCUMENT) private document: Document,
-              private leftHandMenuService: LeftHandMenuService) {
+              private leftHandMenuService: LeftHandMenuService,
+              private chainService: ChainService,
+              private utilsService: UtilsService) {
   }
 
   ngOnInit(): void {
   }
 
   ngAfterViewInit(): void {
-    let barsMenuIcon = document.getElementById('btn-bars-menu');
+    const barsMenuIcon = document.getElementById('btn-bars-menu');
     barsMenuIcon?.classList.remove('hide');
-    let socialIcons = document.getElementsByClassName('btn-social');
+    const socialIcons = document.getElementsByClassName('btn-social');
     if (socialIcons && socialIcons.length) {
       for (let i = 0; i < socialIcons.length; i++) {
         socialIcons.item(i)?.classList.add('hide');
       }
     }
+    if (this.chain) {
+      this.chainService.getChainStatus(this.chain)
+        .subscribe({
+          next: (data: any) => {
+            const latestBlockHeight = data.result.sync_info.latest_block_height;
+            const latestBlockTime = data.result.sync_info.latest_block_time;
+            const latestBlockTimeDate = new Date(latestBlockTime);
+            const now = new Date();
+            const timeDifferenceInSeconds = (now.getTime() - latestBlockTimeDate.getTime()) / 1000;
+            if (timeDifferenceInSeconds > 600) { // 10 minutes
+              this.chainStatus = ChainStatus.HALTED;
+              this.chainStatusMessage = `Chain is halted ${this.utilsService.secondsToHumanReadableFormat(timeDifferenceInSeconds)}
+               ago, the latest block height: ${latestBlockHeight}`
+              return;
+            }
+            this.chainStatus = ChainStatus.SYNCED;
+            this.chainStatusMessage = this.chain?.summaryDisabled
+              ? `Chain is synced, the latest block height: ${latestBlockHeight}`
+              : 'Chain is synced';
+          },
+          error: (error: any) => {
+            this.chainStatus = ChainStatus.INACTIVE;
+            this.chainStatusMessage = 'Our RPC node is down, apologies for inconvenience.';
+          }
+        });
+    }
   }
 
   ngOnDestroy(): void {
-    let barsMenuIcon = document.getElementById('btn-bars-menu');
+    const barsMenuIcon = document.getElementById('btn-bars-menu');
     barsMenuIcon?.classList.add('hide');
-    let socialIcons = document.getElementsByClassName('btn-social');
+    const socialIcons = document.getElementsByClassName('btn-social');
     if (socialIcons && socialIcons.length) {
       for (let i = 0; i < socialIcons.length; i++) {
         socialIcons.item(i)?.classList.remove('hide');
